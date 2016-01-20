234ffaab93d6c06b339f4bcd8b2ea7a76cea017a
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-01-20 18:17:03 +0900

    implement in Clojure & Scala

---
+++ src/main/clojure/chapter01/core.clj
@@ -0,0 +1,16 @@
+(ns chapter01.core)
+
+(defrecord Book [name])
+
+(deftype Bookshelf [books]
+  clojure.lang.Seqable
+  (seq [this]
+    (seq (.books this))))
+
+(defn -main [& args]
+  (let [bookshelf (->Bookshelf [(->Book "Around the World in 80 Days")
+                                (->Book "Bible")
+                                (->Book "Cinderella")
+                                (->Book "Daddy-Long-Legs")])]
+    (doseq [book bookshelf]
+      (println (.name book)))))
---
+++ src/main/scala/chapter01/Main.scala
@@ -0,0 +1,21 @@
+package chapter01
+
+case class Book(name: String)
+
+class Bookshelf(books: Book*) extends Traversable[Book] {
+  def foreach[U](f: Book => U) = {
+    books.foreach(f)
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val bookshelf = new Bookshelf(
+      Book("Around the World in 80 Days"),
+      Book("Bible"),
+      Book("Cinderella"),
+      Book("Daddy-Long-Legs")
+    )
+    for (book <- bookshelf) println(book.name)
+  }
+}
